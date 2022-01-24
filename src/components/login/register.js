8ba2a5d3673a38adc7c7c680bb8e8cc252c82bfd
banadoras <64110552+banadoras@users.noreply.github.com>
2022-01-23 20:42:45 -0500

    a9

--- src/components/login/register.js
+++ src/components/login/register.js
@@ -64,7 +64,17 @@ export default function Register() {
     <div className="register">
       <h4>Register</h4>
       <form onSubmit={handleSubmit}>
+        <img
+          src={
+            file
+              ? URL.createObjectURL(file)
+              : "https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+          }
+          alt="profile"
+        />
+        <label for="file-photo">Upload Photo</label>
         <input
+          id="file-photo"
           type="file"
           name="photo"
           onChange={(e) => {
